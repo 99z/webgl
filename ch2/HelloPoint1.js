6843075a73d88896141faaccf5bc8b844cc89af1
Nicolas S. Montanaro <nsmon@rbox.co>
2018-10-06 16:47:15 -0400

    update

--- ch2/HelloPoint1.js
+++ ch2/HelloPoint1.js
@@ -1,12 +1,21 @@
 function main() {
-    let canvas = document.getElementById('webgl');
-    let gl = canvas.getContext('webgl');
+	let canvas = document.getElementById('webgl');
+	let gl = canvas.getContext('webgl');
 
-    if (!gl) {
-	console.log('Failed to get the rendering context');
-	return;
-    }
+ 	if (!gl) {
+ 		console.log('Failed to get the rendering context');
+		return;
+	}
 
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+	let vertexShaderSrc = document.getElementById('2d-vertex-shader').text;
+	let fragShaderSrc = document.getElementById('2d-fragment-shader').text;
+	
+	if (!initShaders(gl, vertexShaderSrc, fragShaderSrc)) {
+		console.log('Failed to initialize shaders.');
+		return;
+	}
+	
+	gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+	gl.drawArrays(gl.POINTS, 0, 1);
 }
